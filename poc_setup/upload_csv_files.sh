107ded52f7f5fe96d7ed1608167ad255964e3865
eddaou issam <64511136+eddaouissam@users.noreply.github.com>
2024-07-30 09:35:45 +0200

    Release 1.0 - Add Shell Scripts for initial upload

--- poc_setup/upload_csv_files.sh
+++ poc_setup/upload_csv_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Definir schema de la source et le stage
-source_path="/mnt/c/Temp/CSV_Files"
+source_path="/////"
 stage_path="@SAP_CSV_FILES"
 
 # Boucle pour chaque fichier present
